28625eed5df219dc082268834269c4297802f23b
codercsx <83553175@qq.com>
2024-09-11 19:46:22 +0800

    chore: button

---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <component>
+    <button>
+      <slot></slot>
+    </button>
+  </component>
+</template>
+<script setup lang="ts">
+import { defineOptions } from 'vue';
+
+defineOptions({
+  name: 'Button',
+})
+</script>
