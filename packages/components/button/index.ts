d3db1554e6fe568fc5408a530a7bafd56ca3a1c7
codercsx <83553175@qq.com>
2024-10-14 15:17:58 +0800

    feat: 添加whitInstall方法

--- packages/components/button/index.ts
+++ packages/components/button/index.ts
@@ -1,6 +1,7 @@
+import type { SFCWithInstall } from '@nosc/utils'
 import { withInstall } from '@nosc/utils'
 import Button from './src/button.vue'
 
-export const NButton = withInstall(Button)
+export const NButton: SFCWithInstall<typeof Button> = withInstall(Button)
 
 export default NButton
--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -1,3 +1,21 @@
-export function withInstall(main: any) {
-  return main
+import type { SFCWithInstall } from './typescript'
+
+export function withInstall<A, B extends Record<string, any>>(main: A, extra?: B) {
+  (main as SFCWithInstall<A>).install = (app): void => {
+    for (const comp of [main, ...Object.values(extra || {})]) {
+      app.component(comp.name, comp)
+    }
+  }
+
+  if (extra) {
+    for (const [key, comp] of Object.entries(extra)) {
+      main[key] = comp
+    }
+  }
+
+  return main as SFCWithInstall<A> & B
+}
+
+export function foo() {
+  return 'foo'
 }
