f67aed60595b0221e96b7694d6a60bb91c032b33
codercsx <83553175@qq.com>
2024-11-17 01:39:36 +0800

    chore: less

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    'rules': {
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'no-unused-vars': ['warn'],
+      'no-console': 'warn'
+    }
+  }
+]
