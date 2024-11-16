40828c902edfca1b0d3af4af3b43bc605c4c8873
codercsx <83553175@qq.com>
2024-11-17 02:01:34 +0800

    feat: eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
-export default [
-  {
-    'rules': {
-      'semi': ['error', 'never'],
-      'quotes': ['error', 'single'],
-      'indent': ['error', 2],
-      'no-unused-vars': ['warn'],
-      'no-console': 'warn'
-    }
-  }
-]
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  ignores: ['/dist', '/node_modules', '/packages/**/dist', '/packages/**/node_modules'],
+  rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'no-unused-vars': ['warn'],
+    'no-console': 'warn',
+  },
+})
