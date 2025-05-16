65f5b5be51f8e6c42176afd2cfded11fa3aad2ad
codercsx <83553175@qq.com>
2025-05-16 15:45:53 +0800

    feat: dialog开发

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { Dialog } from '@nosc/components'
+import { ref } from 'vue';
+
+const visibility = ref(false)
+const visibilityClick = () => {
+  visibility.value = !visibility.value
+}
 
 </script>
 
@@ -9,9 +15,19 @@ import { Dialog } from '@nosc/components'
     <button class="rounded-md
       bg-black/20 px-4 py-2 text-sm font-medium text-white
       hover:bg-black/30 focus:outline-none focus-visible:ring-2
-      focus-visible:ring-white/75 cursor-pointer">
+      focus-visible:ring-white/75 cursor-pointer" @click="visibilityClick">
       编辑
     </button>
-    <Dialog class="relative z-10" />
+    <Dialog :open="visibility" class="absolute z-10 bg-white rounded-2xl w-100 h-50 text-center of-hidden">
+      <div class="h-5 bg-blue/50"></div>
+      <div></div>
+      <div class="inline-flex justify-center rounded-md
+        border border-transparent bg-blue-100 px-4 py-2
+        text-sm font-medium text-blue-900 hover:bg-blue-200
+        focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500
+        focus-visible:ring-offset-2 cursor-pointer">
+        确认
+      </div>
+    </Dialog>
   </div>
 </template>
